fb952c405e55965f8d021644fb30d40f9279d04f
Orkid <mrs.emadi@yahoo.com>
2021-04-13 14:34:23 +0430

    ADD: FirebaseConfig

--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,11 @@ import firebase from 'firebase/app';
 
 const firebaseConfig = {
     apiKey:  process.env.REACT_APP_APIKEY,
-    authDomain: "firestore-demo-c7e9d.firebaseapp.com",
-    projectId: "firestore-demo-c7e9d",
-    storageBucket: "firestore-demo-c7e9d.appspot.com",
-    messagingSenderId: "510373461616",
-    appId: "1:510373461616:web:6b6333e9e0c1071c593eb5"
+    authDomain:  process.env.REACT_APP_AUTHDOMAIN ,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket:  process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId:  process.env.REACT_APP_MESSAGINGSENDERID ,
+    appId: process.env.REACT_APP_APPID
 };
 
 
